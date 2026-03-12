d9645af4c5d7b1c243a85fea1f95ee3a52be0b2f
DFCEO <50876790+DFCEO@users.noreply.github.com>
2026-03-12 17:06:50 +0800

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  // 静态导出配置（可选，如果要部署到静态托管）
+  // output: 'export',
+  // distDir: 'dist',
+};
+
+export default nextConfig;
